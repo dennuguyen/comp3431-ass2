b715eb2d568af5ad55973bae452a38432f42dbf3
Ubuntu <ubuntu@rafiki.cse.unsw.EDU.AU>
2020-11-09 03:01:58 +0000

    Wrote first pass test for intersection stopping

--- ass2_test/scripts/stop.py
+++ ass2_test/scripts/stop.py
@@ -8,21 +8,9 @@ def callback_road_info(data):
     # Five number array
     road_info_data = data.data
     msg = Twist()
-    if road_info_data[2] < 20:
-        msg.linear.x = 0
-    elif road_info_data[2] < 80:
-        msg.linear.x = 0.16 * road_info_data[2] / 190
-    else:
-        msg.linear.x = 0.16
+    msg.linear.x = 0
 
-    
-    if road_info_data[1] - road_info_data[3] < 10:
-        msg.angular.z = -0.1
-    elif road_info_data[1] - road_info_data[3] > 10:
-        msg.angular.z = 0.1
-    else:
-        msg.angular.z = 0
-    
+    msg.angular.z = 0
     global cmd_vel
     cmd_vel.publish(msg)
 
@@ -30,6 +18,6 @@ if __name__ == '__main__':
     rospy.Subscriber('/road_info', RoadInfo, callback_road_info)
     global cmd_vel
     cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    rospy.init_node('ass2_cpu', anonymous=True)
+    rospy.init_node('stop', anonymous=True)
     rospy.spin()
 
